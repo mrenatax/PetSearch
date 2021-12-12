6eef83a0f1063626d3631a82a3c9865da05a24a7
mrenatax <renatamansurova2001@gmail.com>
2021-12-12 16:53:05 +0300

    filtering and sorting for age added

--- src/main/java/com/nc/petSearch/repository/PetsDBRepo.java
+++ src/main/java/com/nc/petSearch/repository/PetsDBRepo.java
@@ -3,7 +3,6 @@ package com.nc.petSearch.repository;
 import com.nc.petSearch.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
