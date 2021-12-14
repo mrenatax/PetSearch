f9259d091759494932db9432c52502f5f4bf2a8f
Olga Pushkina <emmanuilaklein@gmail.com>
2021-12-14 20:53:01 +0300

    ''

---
+++ src/main/java/com/nc/petSearch/entity/User.java
@@ -0,0 +1,28 @@
+package com.nc.petSearch.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+//@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @ManyToMany
+    private Set<Role> roles;
+}
