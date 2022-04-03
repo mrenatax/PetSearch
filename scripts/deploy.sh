f3adfb40ee7c5ae7d9b14a772a86aa8e1efc7423
mrenatax <rm.billy75@gmail.com>
2022-04-03 20:13:30 +0300

    updated jar name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,14 @@ mvn clean package
 echo 'Copy files...'
 
 scp -i ~/.ssh/id_rsa \
-    target/petShop.jar \
+    target/PetSearch-1.0-SNAPSHOT.jar \
     mrenatax@51.250.96.123:/home/mrenatax/
 
 echo 'Restart server...'
 
 ssh -i ~/.ssh/id_rsa mrenatax@51.250.96.123 << EOF
 pgrep java | xargs kill -9
-nohup java -jar petShop.jar > log.txt &
+nohup java -jar PetSearch-1.0-SNAPSHOT.jar > log.txt &
 EOF
 
 echo 'Bye'
