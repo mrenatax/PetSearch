5210d40c71761d5938c9c330cf9eef2861bb4ccb
mrenatax <rm.billy75@gmail.com>
2022-04-03 18:49:53 +0300

    changed ipv4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/new_ssh \
+scp -i ~/.ssh/id_rsa \
     target/petShop.jar \
-    mrenatax@51.250.102.166:/home/mrenatax/
+    mrenatax@51.250.96.123:/home/mrenatax/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/new_ssh mrenatax@51.250.102.166 << EOF
+ssh -i ~/.ssh/id_rsa mrenatax@51.250.96.123 << EOF
 pgrep java | xargs kill -9
 nohup java -jar petShop.jar > log.txt &
 EOF
