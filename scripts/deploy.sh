b0e8dd362486cba0147af44f47aefe857049f8b4
mrenatax <rm.billy75@gmail.com>
2022-04-03 18:36:09 +0300

    deploy settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/id_rsa/pet_rsa \
+scp -i ~/.ssh/new_ssh \
     target/petShop.jar \
-    mrenatax@192.168.1.160:/home/mrenatax/
+    mrenatax@51.250.102.166:/home/mrenatax/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa/pet_rsa mrenatax@192.168.1.160 << EOF
+ssh -i ~/.ssh/new_ssh mrenatax@51.250.102.166 << EOF
 pgrep java | xargs kill -9
 nohup java -jar petShop.jar > log.txt &
 EOF
